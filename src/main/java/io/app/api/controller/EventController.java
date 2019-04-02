3ce56a3e7b6c342222eeb6aacac75d9074b92c41
Chubasic <joebereza@gmail.com>
2019-04-02 23:07:59 +0300

    Mirgrated to Gradle

--- src/main/java/io/app/api/controller/EventController.java
+++ src/main/java/io/app/api/controller/EventController.java
@@ -1,13 +1,13 @@
 package io.app.api.controller;
 
-import io.app.api.model.Event;
-import io.app.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.app.api.model.Event;
+import io.app.api.service.*;
+
 import java.util.List;
 import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/events")
@@ -18,21 +18,16 @@ public class EventController {
 	@GetMapping("/all")
 	private List<Event> getAllEvents() {
 		return eventService.getAllEvents();
-//		return Flux.fromIterable(eventService.getAllEvents());
 	}
-
 
 	@GetMapping("/{id}")
 	private Optional<Event> getEvent(@PathVariable String id) {
 		return eventService.getOneEvent(id);
-//		return Mono.justOrEmpty(eventService.getOneEvent(id));
 	}
-
 
 	@PostMapping("/new")
 	private List<Event> addEvent(@RequestBody Event newEvent) {
 		eventService.addTopic(newEvent);
-//		return Flux.fromIterable(eventService.getAllEvents());
 		return eventService.getAllEvents();
 	}
 
